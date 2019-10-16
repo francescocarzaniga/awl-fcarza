df06b873fa7c6b6b62a50fcffda2245fe771658c
root <francesco.carzaniga@gmail.com>
2019-10-16 12:00:52 +0000

    Try to fix

--- default.nix
+++ default.nix
@@ -6,6 +6,11 @@
 # commands such as:
 #     nix-build -A mypackage
 
+let
+  nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+in
 { pkgs ? import <nixpkgs> {} }:
 
 {
